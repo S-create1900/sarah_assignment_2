9daa9d981e2ac4d3be2604d2e03efb7e203ebef7
S-create1900 <saphron_m@yahoo.com>
2024-11-10 10:16:20 -0700

    Ready for submittal
    Fixed the previous issues.

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -15,6 +15,7 @@ public class Assignment2Application {
 	
 // Create a variable to hold the Random #.  Need to add 1 to ensure range is from 1 - 100 and not 0-99.	
 	int randomNumber = rand.nextInt(100) + 1;
+	
 	int attempts = 0;     // counter for attempts
 	int maxAttempts = 5;  // Set the maximum number of attempts
 
@@ -22,31 +23,29 @@ public class Assignment2Application {
 //  Prompt user to enter their guess = System,out.println("");  string
 	System.out.println("Welcome, Willing To Try Your Luck?");
 	System.out.println("Pick a number between 1 and 100:");
+//cheating to know random # to check if if/else statements are working correctly &  Put this after final guess
 	System.out.println("Random number is " + randomNumber);
 	
-	//  Adding a while loop: ( do not put the random # generation within the while loop to maintain the same random 
-//	number between given guesses.
+//  Adding a while loop: do not put the random # generation within the while loop to maintain the same random number between guesses.
 	while(attempts < maxAttempts) {
 		attempts++;
 //	
 	
-//Get a int from user, and store that in a variable into playerGuess.
+//Get a numeric guess from user, and store that in a variable into playerGuess.
 	int playerGuess = scanner.nextInt();
 
 	
 // Now we need to determine if the playerGuess is higher or lower than the Random # using if/else statements.
+
+	if (playerGuess == randomNumber) {
+		System.out.println("You win!");
+		break;	// Stops the game if winning # is picked.
+		}
+		
 	if (attempts == maxAttempts) {
-		if (playerGuess == randomNumber) {
-			System.out.println("You win!");
-		break;
-	// Stops the game if winning # is picked.
-		}else {
-	        System.out.println("You lose, the number to guess was: " + randomNumber);
-	    }
-			
-	}
-// if it is not the final attempt, give feedback to pick higher or lower	
-	if (randomNumber > playerGuess) {
+	    System.out.println("You lose, the number to guess was: " + randomNumber);
+// if not on the final attempt, give feedback to pick higher or lower	
+	}else if (playerGuess < randomNumber) {
 		System.out.println("Please pick a higher number");
 	}else{
 		System.out.println("Please pick a lower number");
@@ -54,8 +53,8 @@ public class Assignment2Application {
 	}
 	scanner.close();
 	}
-
 }
+
 
 //cheating to know random # to check if if/else statements are working correctly &  Put this after final guess
 //	System.out.println("Random number is " + randomNumber);
